f0aad49265df2fc8ad72129e5764d5eb0c3d2b42
Bmar21 <brandonmalor21@gmail.com>
2016-09-21 11:41:57 -0600

    Fixed repo

---
+++ UIplayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var DynamicView=UIView(frame: CGRectMake(100, 200, 100, 200))
+DynamicView.backgroundColor=UIColor.blueColor()
+DynamicView.layer.cornerRadius=25
+DynamicView.layer.borderWidth=2
+self.view.addSubview(DynamicView)
+
